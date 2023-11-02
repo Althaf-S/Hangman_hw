232f7f69f5db11a08637b310fbb47664740f8c39
Althaf S <althaf.sabsara@gmail.com>
2023-11-03 00:38:45 +0530

    RED : Happens because test function not returning masked string

--- hangman_test.py
+++ hangman_test.py
@@ -25,12 +25,17 @@ def test_random_word_isalphabet():
 def test_random_word_length():
   fname = "/tmp/simplewords"
   with open(fname,"w") as f:
-    f.writelines(["Grape\n", "apple\n", "mang-o's\n", "He\n", "she\n"])
+    f.writelines(["Grape\n", "apple\n", "mang-o's\n", "he\n", "she\n"])
     
   for _ in range(100):
     assert hangman.get_a_word(fname) == "apple"
     
   os.unlink(fname)
+  
+def test_masking_the_word():
+  for _ in range(100):
+    assert hangman.masking_the_word("apple") == "-----"
+    
 
 
   
