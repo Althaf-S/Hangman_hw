a6d16f09b22ff755b481a6e089adb9c5f69aa854
Althaf S <althaf.sabsara@gmail.com>
2023-11-02 18:22:02 +0530

    GREEN : Resolution of Failure due to capital letter in word

--- hangman.py
+++ hangman.py
@@ -1,2 +1,13 @@
-def get_a_word(word):
-  return word
+import random
+
+def get_a_word(wordlist="/usr/share/dict/words"):
+    good_words = []
+    with open(wordlist) as f:
+        words = [x.strip() for x in f]
+        for word in words:
+            if not word.islower(): 
+                continue
+            good_words.append(word)
+
+        return random.choice(good_words)
+
