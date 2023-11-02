73c56fb1387e7cf5f221ac5353c698e4424eb078
Althaf S <althaf.sabsara@gmail.com>
2023-11-02 18:21:01 +0530

    RED : Failure happend due to a capital letter in word

--- hangman_test.py
+++ hangman_test.py
@@ -1,4 +1,19 @@
+import os
+
 import hangman
 
-def test_word():
-  assert hangman.get_a_word("apple") == "apple"
+def test_random_word_lowercase():
+    fname = "/tmp/sample_wordlist"
+    with open(fname, "w") as f:
+        f.writelines(["Grape\n", "apple\n", "Mango\n"])
+        
+    for _ in range(100):
+        assert hangman.get_a_word(fname) == "apple"
+
+    os.unlink(fname)
+
+
+
+  
+  
+
